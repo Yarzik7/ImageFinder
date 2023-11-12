c7b27cb3c7894d3dee42943471348106c1c0c456
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-11-12 08:17:07 +0200

    add configures

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const ref = {
+  form: document.querySelector(".search-form"),
+  searchBtnEl: document.querySelector(".header__button-search"),
+  loadMoreEl: document.querySelector(".button-load-more"),
+  galleryEl: document.querySelector(".gallery"),
+};
